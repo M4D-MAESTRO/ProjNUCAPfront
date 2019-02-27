50930fe0f243538595c1850db7780b139c586536
Henrique de Castro <lhccorrea@hotmail.com>
2019-02-27 15:50:28 -0300

    Correção no intercepction

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,40 +1,32 @@
-import { StorageService } from '../../services/storage.file';
-import { Injectable } from "@angular/core";
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { Observable, throwError } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable } from 'rxjs'; // IMPORTANTE: IMPORT ATUALIZADO
 import { catchError } from 'rxjs/operators';
- 
- 
+
+
 @Injectable()
-export class ErrorInterceptor implements HttpInterceptor{
- 
-    constructor(public storage: StorageService){ }
- 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-        console.log("passou")
-        return next.handle(req)
-                .pipe(
-                    catchError(error => {
-                       if( !error.status ){
-                            error = JSON.parse(error);
-                        }
-                        switch(error.status){
-                            case 403: this.handle403();
-                            break;
-                        }
- 
-                        return throwError(error);
-                    })) as any;
+export class ErrorInterceptor implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log("Passou no interceptor");
+        return next.handle(req).pipe(catchError((error, caught) => {
+
+            let errorObj = error;
+            if (errorObj.error) {
+                errorObj = errorObj.error;
+            }
+            if (!errorObj.status) {
+                errorObj = JSON.parse(errorObj);
+            }
+
+            console.log("Erro detectado pelo interceptor:");
+            console.log(errorObj);
+
+            return Observable.throw(errorObj);
+        })) as any;
     }
- 
- 
-handle403(){
-        this.storage.setLocalUser(null);
-    }
- 
 }
- 
- 
+
 export const ErrorInterceptorProvider = {
     provide: HTTP_INTERCEPTORS,
     useClass: ErrorInterceptor,
